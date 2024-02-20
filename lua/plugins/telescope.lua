90701cbd1ab02ca20060b577f970dc89cf92fa5d
fikrianamm <fikritaufiqulanam1@gmail.com>
2024-02-20 13:19:08 +0700

    [feat] setup telescope to ignore folder node_modules

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,4 +5,11 @@ return {
     { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<leader>i", "<cmd>Telescope import<cr>", desc = "Import File" },
   },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        file_ignore_patterns = { "./node_modules/*", "node_modules", "^node_modules/*", "node_modules/*" },
+      },
+    })
+  end,
 }
